fbfc6e65b2170d255001e3e1009e4355f3ce7abc
Masashi Yoshida <mysd.33@gmail.com>
2020-04-05 07:30:49 +0900

    引数なしのLogicのインスタンス生成にも対応

--- sparkFramework/src/main/scala/com/example/fw/domain/logic/LogicCreator.scala
+++ sparkFramework/src/main/scala/com/example/fw/domain/logic/LogicCreator.scala
@@ -1,22 +1,38 @@
 package com.example.fw.domain.logic
 
 import com.example.fw.domain.dataaccess.DataFileReaderWriter
+
+import scala.reflect.api.JavaUniverse
 import scala.reflect.runtime.{universe => ru}
 
 object LogicCreator {
-  def newInstance[T](classname: String, dataFileReaderWriter: DataFileReaderWriter[T]): Logic = {
-    //参考
-    //https://kazuhira-r.hatenablog.com/entry/20130121/1358780334
-    //https://docs.scala-lang.org/ja/overviews/reflection/overview.html
+  //参考
+  //https://kazuhira-r.hatenablog.com/entry/20130121/1358780334
+  //https://docs.scala-lang.org/ja/overviews/reflection/overview.html
+
+  //引数あり版
+  //Logicのコンストラクタに引数があれえばDataFileReaderWriterを渡す前提
+  def newInstance[T](className: String, dataFileReaderWriter: DataFileReaderWriter[T]): Logic = {
+    val constructorMirror: ru.MethodMirror = getConstructorMirror(className)
+    val paramList = constructorMirror.symbol.asMethod.paramLists
+    val logic = if (paramList.size == 0) {
+      constructorMirror()
+    } else if(paramList.size == 1) {
+      constructorMirror(dataFileReaderWriter)
+    } else {
+      //TODO:引数0か1の前提になってしまっている
+      ???
+    }
+    logic.asInstanceOf[Logic]
+  }
+
+  private def getConstructorMirror(className: String) = {
     val mirror = ru.runtimeMirror(getClass.getClassLoader)
-    val classSymbol = mirror.staticClass(classname)
+    val classSymbol = mirror.staticClass(className)
     val classMirror = mirror.reflectClass(classSymbol)
     val constructorSymbol = classSymbol.typeSignature.decl(ru.termNames.CONSTRUCTOR).asMethod
     val constructorMirror = classMirror.reflectConstructor(constructorSymbol)
-    //Logicのコンストラクタは1つ引数としてDataFileReaderWriterを渡す前提
-    //TODO:本当は引数1つのコンストラクタがあることをチェックすると良い
-    val logic = constructorMirror(dataFileReaderWriter)
-    logic.asInstanceOf[Logic]
+    constructorMirror
   }
 
 }
