407022dfe41ffb14076d1df17ef45afb1f9ac4aa
Masashi Yoshida <mysd.33@gmail.com>
2020-04-08 17:34:07 +0900

    DatabricksDataFileReaderWriterのDataset実装

--- sparkFramework_databricks/src/main/scala/com/example/fw/infra/dataaccess/DatabricksDataFileReaderWriter.scala
+++ sparkFramework_databricks/src/main/scala/com/example/fw/infra/dataaccess/DatabricksDataFileReaderWriter.scala
@@ -9,18 +9,23 @@ import scala.reflect.runtime.universe
 trait DatabricksDataFileReaderWriter extends StandardSparkDataFileReaderWriter {
   override def readToDf(inputFile: DataFile[Row], sparkSession: SparkSession): DataFrame = {
     inputFile match {
-      //DeltaLakeのみReaderWriteを差し替え
+      //DeltaLakeのみReaderWriterを差し替え
       case f: ParquetModel[Row] => new DeltaLakeReaderWriter().readToDf(f, sparkSession)
       case _ => super.readToDf(inputFile, sparkSession)
     }
   }
 
-  //TODO: 実装
-  override def readToDs[T <: Product : universe.TypeTag](inputFile: DataFile[T], sparkSession: SparkSession): Dataset[T] = ???
+  override def readToDs[T <: Product : universe.TypeTag](inputFile: DataFile[T], sparkSession: SparkSession): Dataset[T] = {
+    inputFile match {
+      //DeltaLakeのみReaderWriterを差し替え
+      case f: ParquetModel[T] => new DeltaLakeReaderWriter().readToDs(f, sparkSession)
+      case _ => super.readToDs(inputFile, sparkSession)
+    }
+  }
 
   override def writeFromDsDf[T](ds: Dataset[T], outputFile: DataFile[T], saveMode: SaveMode): Unit = {
     outputFile match {
-      //DeltaLakeのみReaderWriteを差し替え
+      //DeltaLakeのみReaderWriterを差し替え
       case f: ParquetModel[T] => new DeltaLakeReaderWriter().writeFromDsDf(ds, f, saveMode)
       case _ => super.writeFromDsDf(ds, outputFile, saveMode)
     }
