969ac651b77768150957a4ff2a0568627b5a2f14
Masashi Yoshida <mysd.33@gmail.com>
2020-04-08 17:17:10 +0900

    プロパティファイルによるファイルパスの切り替え改善

--- sparkFramework/src/main/scala/com/example/fw/domain/utils/ResourceBundleManager.scala
+++ sparkFramework/src/main/scala/com/example/fw/domain/utils/ResourceBundleManager.scala
@@ -3,11 +3,21 @@ package com.example.fw.domain.utils
 import java.util.ResourceBundle
 
 object ResourceBundleManager {
+  private val PROPERTIES_FILE_NAME = "application"
+  private val ACTIVE_PROFILE_KEY = "active.profile"
+  private val SEPARATOR = "-"
+
   def get(key :String): String = {
-    //TODO:プロパティがなかった時等の異常系の対処
-    val rb = ResourceBundle.getBundle("application")
-    val profile = rb.getString("active.profile")
-    val rbProfile = ResourceBundle.getBundle("application-" + profile)
-    rbProfile.getString(key)
+    val rb = ResourceBundle.getBundle(PROPERTIES_FILE_NAME)
+    val profile = rb.getString(ACTIVE_PROFILE_KEY)
+    val rbProfile = ResourceBundle.getBundle(PROPERTIES_FILE_NAME + SEPARATOR + profile)
+    val value = if (rb.containsKey(key)) {
+      rb.getString(key)
+    } else if (rbProfile.containsKey(key)) {
+      rbProfile.getString(key)
+    } else {
+      null
+    }
+    value
   }
 }
