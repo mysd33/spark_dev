6dcc8b0076de5c34367134660914c4fa51040485
Masashi Yoshida <mysd.33@gmail.com>
2020-04-08 21:20:02 +0900

    Databricksのエントリポイントの修正

---
+++ sparkFramework_databricks/src/main/scala/com/example/fw/app/DatabricksConnectApplicationEntryPoint.scala
@@ -0,0 +1,44 @@
+package com.example.fw.app
+
+import com.example.fw.domain.dataaccess.DataFileReaderWriter
+import com.example.fw.domain.logic.LogicCreator
+import com.example.fw.domain.utils.ResourceBundleManager
+import com.example.fw.domain.utils.Using.using
+import com.example.fw.infra.dataaccess.DatabricksDataFileReaderWriter
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
+object DatabricksConnectApplicationEntryPoint {
+  def main(args: Array[String]): Unit = {
+    assert(args.length > 0)
+    //TODO: コンストラクタ引数をとれるようにする
+    val appName = args(0)
+
+    //TODO:独自のプロパティではなくてSpark実行時のパラメータのほうがよいか？
+    val clusterMode = ResourceBundleManager.get("clustermode")
+    val logLevel = ResourceBundleManager.get("loglevel")
+
+    //Sparkの実行
+    using(SparkSession.builder()
+      .master(clusterMode)
+      .appName(appName)
+      .getOrCreate()
+    ) { spark =>
+      val sc = spark.sparkContext
+      //TODO:ログが多いのでオフしている。log4j.propertiesで設定できるようにするなど検討
+      sc.setLogLevel(logLevel)
+      Logger.getLogger("org").setLevel(Level.OFF)
+      Logger.getLogger("akka").setLevel(Level.OFF)
+
+      //Logicインスタンスの実行
+      val logic = LogicCreator.newInstance(appName, createDataFileReaderWriter())
+      logic.execute(spark)
+    }
+  }
+
+  private def createDataFileReaderWriter(): DataFileReaderWriter = {
+    //Databricks用のReaderWriterに差し替え
+    new DataFileReaderWriter with DatabricksDataFileReaderWriter
+  }
+
+}
--- sparkFramework_databricks/src/main/scala/com/example/fw/app/DatabricksJobApplicationEntryPoint.scala
+++ sparkFramework_databricks/src/main/scala/com/example/fw/app/DatabricksJobApplicationEntryPoint.scala
@@ -3,12 +3,11 @@ package com.example.fw.app
 import com.example.fw.domain.dataaccess.DataFileReaderWriter
 import com.example.fw.domain.logic.LogicCreator
 import com.example.fw.domain.utils.ResourceBundleManager
-import com.example.fw.domain.utils.Using.using
 import com.example.fw.infra.dataaccess.DatabricksDataFileReaderWriter
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 
-object DatabricksApplicationEntryPoint {
+object DatabricksJobApplicationEntryPoint {
   def main(args: Array[String]): Unit = {
     assert(args.length > 0)
     //TODO: コンストラクタ引数をとれるようにする
@@ -19,21 +18,22 @@ object DatabricksApplicationEntryPoint {
     val logLevel = ResourceBundleManager.get("loglevel")
 
     //Sparkの実行
-    using(SparkSession.builder()
-      .master(clusterMode)
+    //DatabricksでJARジョブプログラムを実行する場合共有SparkContextを使用するため
+    //SparkSession.close（SparkContext.stop）を呼び出してはいけない
+    //https://docs.microsoft.com/ja-jp/azure/databricks/jobs#%E5%85%B1%E6%9C%89-sparkcontext-%E3%82%92-%E4%BD%BF%E7%94%A8%E3%81%99%E3%82%8B--use-the-shared-sparkcontext
+    val spark = SparkSession.builder()
       .appName(appName)
       .getOrCreate()
-    ) { spark =>
-      val sc = spark.sparkContext
-      //TODO:ログが多いのでオフしている。log4j.propertiesで設定できるようにするなど検討
-      sc.setLogLevel(logLevel)
-      Logger.getLogger("org").setLevel(Level.OFF)
-      Logger.getLogger("akka").setLevel(Level.OFF)
+    val sc = spark.sparkContext
+    //TODO:ログが多いのでオフしている。log4j.propertiesで設定できるようにするなど検討
+    sc.setLogLevel(logLevel)
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
 
-      //Logicインスタンスの実行
-      val logic = LogicCreator.newInstance(appName, createDataFileReaderWriter())
-      logic.execute(spark)
-    }
+    //Logicインスタンスの実行
+    val logic = LogicCreator.newInstance(appName, createDataFileReaderWriter())
+    logic.execute(spark)
+
   }
 
   private def createDataFileReaderWriter(): DataFileReaderWriter = {
