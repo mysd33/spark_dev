1ebd361ad0aa7358ea68853682f238a8cdc9a2e2
Masashi Yoshida <mysd.33@gmail.com>
2020-04-11 11:02:15 +0900

    プロパティ管理機能のプロファイル切り替え機能改善

--- dbconnect_ap/src/main/scala/com/example/app/EntryPoint.scala
+++ dbconnect_ap/src/main/scala/com/example/app/EntryPoint.scala
@@ -1,11 +1,12 @@
 package com.example.app
 
 import com.example.fw.app.DatabricksConnectApplicationEntryPoint
+import com.example.fw.domain.utils.ResourceBundleManager
 import org.apache.spark.SparkContext
 
 object EntryPoint extends DatabricksConnectApplicationEntryPoint {
 
-  //TODO:なんか終了時にINFOだけど、ログがいっぱいでる
+  //TODO:なんか終了時にINFOログがいっぱい出力されてしまうので解析要
   //20/04/11 09:21:27 INFO privateLog: "dbe-client" #65 TIMED_WAITING
 
   def main(args: Array[String]): Unit = {
@@ -18,5 +19,8 @@ object EntryPoint extends DatabricksConnectApplicationEntryPoint {
     sc.addJar("sparkFramework_databricks/target/scala-2.11/sparkframework_databricks_2.11-0.1.jar")
     sc.addJar("dbconnect_ap/target/scala-2.11/dbconnect_app_2.11-0.1.jar")
 
+    //TODO:暫定コード
+    val basePath = ResourceBundleManager.get("basepath")
+    println(s"basePath : $basePath")
   }
 }
--- sparkFramework/src/main/scala/com/example/fw/domain/utils/ResourceBundleManager.scala
+++ sparkFramework/src/main/scala/com/example/fw/domain/utils/ResourceBundleManager.scala
@@ -6,12 +6,25 @@ object ResourceBundleManager {
   private val PROPERTIES_FILE_NAME = "application"
   private val ACTIVE_PROFILE_KEY = "active.profile"
   private val SEPARATOR = "-"
+  private val DEFAULT_PROFILE = "prod"
+  private lazy val rb = ResourceBundle.getBundle(PROPERTIES_FILE_NAME)
+  private lazy val profile = {
+    val sysPropProfile = System.getProperty(ACTIVE_PROFILE_KEY)
+    if (sysPropProfile != null && !sysPropProfile.isEmpty) {
+      sysPropProfile
+    } else if (rb.containsKey(ACTIVE_PROFILE_KEY)) {
+      rb.getString(ACTIVE_PROFILE_KEY)
+    } else {
+      DEFAULT_PROFILE
+    }
+  }
+  private lazy val rbProfile = ResourceBundle.getBundle(PROPERTIES_FILE_NAME + SEPARATOR + profile)
 
-  def get(key :String): String = {
-    val rb = ResourceBundle.getBundle(PROPERTIES_FILE_NAME)
-    val profile = rb.getString(ACTIVE_PROFILE_KEY)
-    val rbProfile = ResourceBundle.getBundle(PROPERTIES_FILE_NAME + SEPARATOR + profile)
-    val value = if (rb.containsKey(key)) {
+  def get(key: String): String = {
+    val tempValue = System.getProperty(key)
+    val value = if (tempValue != null && !tempValue.isEmpty) {
+      tempValue
+    } else if (rb.containsKey(key)) {
       rb.getString(key)
     } else if (rbProfile.containsKey(key)) {
       rbProfile.getString(key)
