40e846c90cf779f68f2f9f156aa7db02bd671f3f
admin <admin@mysd33.work>
2020-04-11 05:01:08 +0000

    Azure Databricksへのマウントコード

---
+++ notebooks/Users/admin@mysd33.work/adls-init.scala
@@ -0,0 +1,53 @@
+// Databricks notebook source
+val appID = "b8d73315-fc1d-495c-9188-9612d86854bc"
+val secret = "qnhVL7.A@Q-O/Y9jAtFAd372nc]:lAt9"
+val tenantID = "94743a8e-f153-4fa4-a334-57e6b14a19d4"
+
+spark.conf.set("fs.azure.account.auth.type", "OAuth")
+spark.conf.set("fs.azure.account.oauth.provider.type", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+spark.conf.set("fs.azure.account.oauth2.client.id", appID)
+spark.conf.set("fs.azure.account.oauth2.client.secret", secret)
+spark.conf.set("fs.azure.account.oauth2.client.endpoint", "https://login.microsoftonline.com/" + tenantID + "/oauth2/token")
+spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "true")
+
+
+// COMMAND ----------
+
+val storageAccountName = "mysd33storage"
+val fileSystemName = "testdata"
+
+spark.conf.set("fs.azure.account.auth.type." + storageAccountName + ".dfs.core.windows.net", "OAuth")
+spark.conf.set("fs.azure.account.oauth.provider.type." + storageAccountName + ".dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+spark.conf.set("fs.azure.account.oauth2.client.id." + storageAccountName + ".dfs.core.windows.net", "" + appID + "")
+spark.conf.set("fs.azure.account.oauth2.client.secret." + storageAccountName + ".dfs.core.windows.net", "" + secret + "")
+spark.conf.set("fs.azure.account.oauth2.client.endpoint." + storageAccountName + ".dfs.core.windows.net", "https://login.microsoftonline.com/" + tenantID + "/oauth2/token")
+spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "true")
+dbutils.fs.ls("abfss://" + fileSystemName  + "@" + storageAccountName + ".dfs.core.windows.net/")
+spark.conf.set("fs.azure.createRemoteFileSystemDuringInitialization", "false")
+
+// COMMAND ----------
+
+val endpoint = "https://login.microsoftonline.com/" + tenantID + "/oauth2/token"
+
+// COMMAND ----------
+
+val configs = Map(
+  "fs.azure.account.auth.type" -> "OAuth",
+  "fs.azure.account.oauth.provider.type" -> "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+  "fs.azure.account.oauth2.client.id" -> appID,
+  "fs.azure.account.oauth2.client.secret" -> secret,
+  "fs.azure.account.oauth2.client.endpoint" -> endpoint)
+
+val mountName = "mystorage"
+
+dbutils.fs.mount(
+  source = "abfss://" + fileSystemName  + "@" + storageAccountName + ".dfs.core.windows.net/",
+  mountPoint = "/mnt/" + mountName,
+  extraConfigs = configs)
+
+// COMMAND ----------
+
+dbutils.fs.ls("/mnt/mystorage")
+
+// COMMAND ----------
+
