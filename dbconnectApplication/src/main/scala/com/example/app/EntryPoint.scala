eb1f5faee937f4750772849c7f05158a07b797f9
Masashi Yoshida <mysd.33@gmail.com>
2020-04-29 15:50:18 +0900

    DatabricksConnectの追加jarの設定の修正

--- dbconnectApplication/src/main/scala/com/example/app/EntryPoint.scala
+++ dbconnectApplication/src/main/scala/com/example/app/EntryPoint.scala
@@ -5,9 +5,9 @@ import com.example.fw.domain.utils.ResourceBundleManager
 import org.apache.spark.SparkContext
 
 object EntryPoint extends DatabricksConnectApplicationEntryPoint {
-
   def main(args: Array[String]): Unit = {
-    //TODO:テストデータをnotebookで、dbutilでDatabricks上にマウントしてから実行する
+    //Databricks上でテストデータをnotebookで、dbutilでDatabricks上にマウントしてから実行すること
+    //notebooks/Users/admin@mysd33.work/adls-init-mount.scalaのコードと、サイトを参考にすること
     //参考
     //https://docs.microsoft.com/ja-jp/azure/azure-databricks/databricks-extract-load-sql-data-warehouse?toc=/azure/databricks/toc.json&bc=/azure/databricks/breadcrumb/toc.json
     //https://docs.microsoft.com/ja-jp/azure/databricks/data/data-sources/azure/azure-datalake-gen2
@@ -15,16 +15,7 @@ object EntryPoint extends DatabricksConnectApplicationEntryPoint {
   }
 
   override protected def addJar(sc: SparkContext): Unit = {
-    // 暫定的に依存関係のjarの追加操作を直接記載
-    sc.addJar("sparkFramework/target/scala-2.11/sparkframework_2.11-0.1.jar")
-    sc.addJar("databricksFramework/target/scala-2.11/databricksFramework_2.11-0.1.jar")
-    sc.addJar("application/target/scala-2.11/application_2.11-0.1.jar")
-    sc.addJar("dbconnectApplication/target/scala-2.11/dbconnectApplication_2.11-0.1.jar")
-
-    //TODO:暫定コード
-    val profile = ResourceBundleManager.getActiveProfile()
-    println(s"Active Profile : $profile")
-    val basePath = ResourceBundleManager.get("basepath")
-    println(s"BasePath : $basePath")
-  }
+    //DatabricksConnectの場合、必要なクラスの入ったjarを追加する必要があるのでsbt assemblyを実行しておくこと
+    sc.addJar("target/scala-2.11/databricks_dev-assembly-0.1.jar")
+ }
 }
