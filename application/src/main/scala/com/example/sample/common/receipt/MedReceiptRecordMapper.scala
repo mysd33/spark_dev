6da57d82a5f7dabf2d6410f158c905759a78784e
Masashi Yoshida <mysd.33@gmail.com>
2020-05-04 15:03:49 +0900

    Receiptのサンプルコードのリファクタリング

---
+++ application/src/main/scala/com/example/sample/common/receipt/MedReceiptRecordMapper.scala
@@ -0,0 +1,50 @@
+package com.example.sample.common.receipt
+
+import org.apache.spark.rdd.RDD
+import scala.reflect.ClassTag
+
+object MedReceiptRecordMapper {
+  private val delimiter = ","
+  private val lineSeparator = "\r\n"
+
+  def mapToReceiptRecordTuples(receipt: String):Array[(String, ReceiptRecord)] = {
+    //1レセプトを改行コードでレコードごとに分解
+    val records = receipt.split(lineSeparator)
+    //最初のMNレコードからレセプト管理番号を取得
+    val receiptKanriNo = MedReceiptRecordMapper.mapToRecord(records(0))
+      .asInstanceOf[MedMNReceiptRecord]
+      .receiptKanriNo
+    //レコードごとのRDDを作成
+    records.map(recordString => {
+      //レコードの種類ごとにファイル分解する前に
+      //どのレセプトかを分かるキーとしてレセプト管理番号を入れておく
+      val record = MedReceiptRecordMapper.mapToRecord(recordString, receiptKanriNo)
+      (record.recordType, record)
+    })
+  }
+
+  def mapToRecord(recordString: String): ReceiptRecord = mapToRecord(recordString, null)
+
+  def mapToRecord(recordString: String, receiptKanriNo: String): ReceiptRecord = {
+    //値がない場合のためlimit=-1
+    val items = recordString.split(delimiter, -1)
+    val unKnownRecord = UnknownReceiptRecord(items)
+    val receiptRecord =
+      unKnownRecord.recordType match {
+        case "MN" => MedMNReceiptRecordMapper.map(items)
+        case "RE" => MedREReceiptRecordMapper.map(receiptKanriNo, items)
+
+        //TODO:レコード種別ごとに、文字列配列をcaseクラスにマッピングする処理を追加していく
+
+        case _ => unKnownRecord
+      }
+    receiptRecord
+  }
+
+  def extractRecordRDD[T <: ReceiptRecord : ClassTag]
+  (recordTypeName: String, rdd: RDD[(String, ReceiptRecord)]): RDD[T] = {
+    rdd.filter(t => t._1 == recordTypeName)
+      .map(t => t._2.asInstanceOf[T])
+  }
+
+}
--- application/src/main/scala/com/example/sample/logic/SampleMedReceiptRDDBLogic.scala
+++ application/src/main/scala/com/example/sample/logic/SampleMedReceiptRDDBLogic.scala
@@ -4,14 +4,13 @@ import com.example.fw.domain.dataaccess.DataFileReaderWriter
 import com.example.fw.domain.logic.RDDToDataFrameBLogic
 import com.example.fw.domain.model.{CsvModel, DataFile, MultiFormatCsvModel}
 import com.example.fw.domain.utils.OptionImplicit._
-import com.example.sample.common.receipt.{MedMNReceiptRecord, MedREReceiptRecord, ReceiptRecord, ReceiptRecordMapper}
+import com.example.sample.common.receipt.{MedMNReceiptRecord, MedREReceiptRecord, ReceiptRecord, MedReceiptRecordMapper}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 class SampleMedReceiptRDDBLogic(dataFileReaderWriter: DataFileReaderWriter)
   extends RDDToDataFrameBLogic(dataFileReaderWriter) {
-  val outputDirPath = "receipt/output"
-  val directoryPath = "/"
+  val outputDirPath = "receipt/output/"
   val re = "RE"
   val mn = "MN"
   var cached: RDD[(String, ReceiptRecord)] = null
@@ -22,47 +21,27 @@ class SampleMedReceiptRDDBLogic(dataFileReaderWriter: DataFileReaderWriter)
       encoding = "MS932") :: Nil
 
   override val outputFiles: Seq[DataFile[Row]] =
-    CsvModel[Row](outputDirPath + directoryPath + re
-    ) :: CsvModel[Row](outputDirPath + directoryPath + mn
+    CsvModel[Row](outputDirPath + re
+    ) :: CsvModel[Row](outputDirPath + mn
     ) :: Nil
 
-
-  //override def process(receipts: RDD[String], sparkSession: SparkSession): RDD[(String, ReceiptRecord)] = {
   override def process(inputs: Seq[RDD[String]], sparkSession: SparkSession): Seq[DataFrame] = {
     import sparkSession.implicits._
-    val lineSeparator = "\r\n"
     val receipts = inputs(0)
     val result = receipts.flatMap(receipt => {
-      //1レセプトを改行コードでレコードごとに分解
-      val records = receipt.split(lineSeparator)
-      //最初のMNレコードからレセプト管理番号を取得
-      val receiptKanriNo = ReceiptRecordMapper.map(null, records(0))
-        .asInstanceOf[MedMNReceiptRecord]
-        .receiptKanriNo
-      //レコードごとのRDDを作成
-      records.map(recordString => {
-        //レコードの種類ごとにファイル分解する前に
-        //どのレセプトかを分かるキーとしてレセプト管理番号を入れておく
-        val record = ReceiptRecordMapper.map(receiptKanriNo, recordString)
-        (record.recordType, record)
-      })
+      MedReceiptRecordMapper.mapToReceiptRecordTuples(receipt)
     })
     //レコード種別ごとにDatasetを作成しファイル出力
-    //明示的に各クラスをキャストして出力しないといけないコードが冗長だが、対処が難しい
     //何度も使用するのでキャッシュしておく
     cached = result.cache()
-    //TODO: レセプト解析コンポーネントとして別クラスに切り出してテストしやすくする
-    val reDS = cached.filter(t => t._1 == re)
-      .map(t => t._2.asInstanceOf[MedREReceiptRecord])
-      .toDF()
-    val mnDS = cached.filter(t => t._1 == mn)
-      .map(t => t._2.asInstanceOf[MedMNReceiptRecord])
-      .toDF()
-    reDS :: mnDS :: Nil
+    val reDF = MedReceiptRecordMapper.extractRecordRDD[MedREReceiptRecord](re, cached).toDF()
+    val mnDF = MedReceiptRecordMapper.extractRecordRDD[MedMNReceiptRecord](mn, cached).toDF()
+    //TODO:レコード種別ごとに処理を追加していく
+    reDF :: mnDF :: Nil
   }
 
   override def tearDown(sparkSession: SparkSession): Unit = {
-    //キャッシュをunpersist
+    //キャッシュを削除
     cached.unpersist()
     super.tearDown(sparkSession)
   }
