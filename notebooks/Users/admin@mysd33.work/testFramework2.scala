5b74010895431da730c8856e1bdcd30b836908c6
admin <admin@mysd33.work>
2020-06-22 05:17:02 +0000

    フレームワークの呼び出し修正

--- notebooks/Users/admin@mysd33.work/testFramework2.scala
+++ notebooks/Users/admin@mysd33.work/testFramework2.scala
@@ -8,10 +8,10 @@ val readerWriter = DatabricksDataModelReaderWriterFactory.createDataModelReaderW
 
 // COMMAND ----------
 
-val logic = new SampleTokuteiXMLDataFrameBLogic(readerWriter)
+//val logic = new SampleTokuteiXMLDataFrameBLogic(readerWriter)
 
 //クラスタにspark-xml関連の依存jarがすべて入っていないと動作しない(spark-xml_2.11.0-0.9.0.jar, txw2-2.3.2.jar)
-//val logic = new SampleXMLDatasetBLogic(readerWriter)
+val logic = new SampleXMLDatasetBLogic(readerWriter)
 //val logic = new SampleTokuteiXMLDatasetBLogic(readerWriter)
 
 
@@ -21,6 +21,9 @@ DatabricksNotebookEntryPoint.run(spark, logic)
 
 // COMMAND ----------
 
-//dbutils.fs.ls("/mnt/mystorage/xml/")
+dbutils.fs.ls("/mnt/mystorage/xml/")
 //dbutils.fs.ls("/mnt/mystorage/tokutei/output/")
-dbutils.fs.ls("/mnt/mystorage/tokutei/output2/")
+//dbutils.fs.ls("/mnt/mystorage/tokutei/output2/")
+
+// COMMAND ----------
+
